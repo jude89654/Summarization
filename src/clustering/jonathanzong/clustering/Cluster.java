6d7eaf4fd5c131262843412f043de235c04a81b9
Jude <jude89654@gmail.com>
2016-07-20 11:14:20 +0800

    clusterer na tuuuuuu...

---
+++ src/clustering/jonathanzong/clustering/Cluster.java
@@ -0,0 +1,11 @@
+package clustering.jonathanzong.clustering;
+
+/**
+ * Created by jude8 on 7/13/2016.
+ */
+public class util {
+}
+
+class Cluster{
+
+}
---
+++ src/clustering/jonathanzong/kmeansClusterer.java
@@ -0,0 +1,7 @@
+package clustering.jonathanzong;
+
+/**
+ * Created by jude8 on 7/19/2016.
+ */
+public class kmeansClusterer {
+}
